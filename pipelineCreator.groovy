4828201a2497c6f7976740b8eebfd62e758a3cdd
Levy Mateus Macedo <levy.macedo.ext@dasa.com.br>
2022-10-28 11:35:30 -0300

    ci: changes in pipelineCreator

--- pipelineCreator.groovy
+++ pipelineCreator.groovy
@@ -5,9 +5,11 @@ multibranchPipelineJob("${Name}") {
     displayName "${Name}"
     description "Builds ${Name}"
     configure {
-        git / 'extensions' / 'hudson.plugins.git.extensions.impl.PathRestriction' {
-            includedRegions "./apps/${Name}/.*"
-            excludedRegions "README.md\n\\.gitignore\npom.xml"
+        it / 'extensions' / 'hudson.plugins.git.extensions.impl.PathRestriction' << {
+            git {
+                includedRegions "./apps/${Name}/.*"
+                excludedRegions "README.md\n\\.gitignore"
+            }
         }
         it / sources / 'data' / 'jenkins.branch.BranchSource' << {
             source(class: 'jenkins.plugins.git.GitSCMSource') {
